1b2ab47bf8b267adeefdc76e8814a829d117980d
Andreas Furster <andreas.furster@hotmail.com>
2016-03-30 15:56:08 +0200

    Knopjes toegevoegd aan het KeuzeScherm

--- Toledo/Toledo.Desktop/KeuzeScherm.Designer.cs
+++ Toledo/Toledo.Desktop/KeuzeScherm.Designer.cs
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.artikelenBeherenBtn = new MetroFramework.Controls.MetroTile();
+            this.verkopenBtn = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
+            // 
+            // artikelenBeherenBtn
+            // 
+            this.artikelenBeherenBtn.ActiveControl = null;
+            this.artikelenBeherenBtn.Location = new System.Drawing.Point(23, 63);
+            this.artikelenBeherenBtn.Name = "artikelenBeherenBtn";
+            this.artikelenBeherenBtn.Size = new System.Drawing.Size(200, 200);
+            this.artikelenBeherenBtn.Style = MetroFramework.MetroColorStyle.Blue;
+            this.artikelenBeherenBtn.StyleManager = null;
+            this.artikelenBeherenBtn.TabIndex = 0;
+            this.artikelenBeherenBtn.Text = "Artikelen beheren";
+            this.artikelenBeherenBtn.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.artikelenBeherenBtn.TileCount = 0;
+            // 
+            // verkopenBtn
+            // 
+            this.verkopenBtn.ActiveControl = null;
+            this.verkopenBtn.Location = new System.Drawing.Point(243, 63);
+            this.verkopenBtn.Name = "verkopenBtn";
+            this.verkopenBtn.Size = new System.Drawing.Size(200, 200);
+            this.verkopenBtn.Style = MetroFramework.MetroColorStyle.Blue;
+            this.verkopenBtn.StyleManager = null;
+            this.verkopenBtn.TabIndex = 1;
+            this.verkopenBtn.Text = "Verkopen";
+            this.verkopenBtn.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.verkopenBtn.TileCount = 0;
             // 
             // KeuzeScherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 371);
+            this.ClientSize = new System.Drawing.Size(466, 281);
+            this.Controls.Add(this.verkopenBtn);
+            this.Controls.Add(this.artikelenBeherenBtn);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.MaximizeBox = false;
             this.Name = "KeuzeScherm";
-            this.Text = "KeuzeScherm";
+            this.Resizable = false;
+            this.Text = "Wat gaan we doen?";
             this.Load += new System.EventHandler(this.KeuzeScherm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile verkopenBtn;
+        private MetroFramework.Controls.MetroTile artikelenBeherenBtn;
     }
 }
