a960113dfdf572bd143073994aa21704662c17be
Andreas Furster <andreas.furster@hotmail.com>
2016-03-30 13:48:26 +0200

    KeuzeScherm toegevoegd

---
+++ Toledo/Toledo.Desktop/KeuzeScherm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toledo.Desktop
+{
+    public partial class KeuzeScherm : Form
+    {
+        public KeuzeScherm()
+        {
+            InitializeComponent();
+        }
+    }
+}
